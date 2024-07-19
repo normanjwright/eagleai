7554d13c84212f59c2b9b810def7245d6bfa7acc
mowenista <ostannard12@gmail.com>
2024-07-19 13:36:12 -0400

    dept func implemented

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def home():
 @app.route("/coursesearch")
 def coursesearch():
     offering = True
-    CSCI_courses = search_courses(courses, "", "AADS",offering, "Cultural Diversity" )
+    CSCI_courses = search_courses(courses, "", "MUSA",offering, "" )
     
     
 
