0697029ab5d6b2bfc363929f25cd4ddf073d54bc
mowenista <ostannard12@gmail.com>
2024-07-23 14:29:05 -0400

    ai start, dummy student testing, student creation func

--- profileStructure.py
+++ profileStructure.py
@@ -1,16 +1,27 @@
 
 
 class Student():
-    def __init__(self, firstname, lastname, school, major, minor, academic_record, grad_year, add_credit, qual_data):
+    def __init__(self, firstname, lastname, school, major, minor, academic_record, curr_year, add_credit, qual_data):
         self.firstname = firstname
         self.lastname = lastname
         self.school = school
         self.major = major
         self.minor = minor
         self.academic_record = academic_record
-        self.grad_year = grad_year
+        self.curr_year = curr_year
         self.add_credit = add_credit
         self.qual_data = qual_data
+
+# I have a test case in the boost.py file
+
+def createStudent(fname, lname, school, major, minor, academic_record, curr_year, add_credit, qual_data):
+    student = Student(fname, lname, school, major, minor, academic_record, curr_year, add_credit, qual_data)
+    return student
+
+
+    
+
+
 
 
 
