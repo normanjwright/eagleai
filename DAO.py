ed3afd524eb81532e57ddaa9c6f2422a7fb043d5
mowenista <ostannard12@gmail.com>
2024-08-05 13:07:48 -0400

    data access functions

--- DAO.py
+++ DAO.py
@@ -57,16 +57,32 @@ student = createStudent(12345678, "Owen", "S",\
 courses, depts = get_all_courses()
 
 def majors_to_string(majors):
-    return
+    str = ""
+    for major in majors:
+        str = str + major + "/"
+    str[:-1]
+    return str
 
 def string_to_majors(str):
-    return
+     # Split the string by "/" to get the list of majors
+    majors = str.split("/")
+    # Remove any empty strings that might occur due to trailing slashes
+    majors = [major for major in majors if major]
+    return majors
 
 def minors_to_string(minors):
-    return
+    str = ""
+    for minor in minors:
+        str = str + minor + "/"
+    str[:-1]
+    return str
 
 def string_to_minors(str):
-    return
+     # Split the string by "/" to get the list of majors
+    minors = str.split("/")
+    # Remove any empty strings that might occur due to trailing slashes
+    minors = [minor for minor in minors if minor]
+    return minors
 
 
 def academic_record_to_string(student):
@@ -98,11 +114,9 @@ def string_to_academic_record(str, course_list):
         
     return academic_record
 
-ar = academic_record_to_string(student)
-new_dict = string_to_academic_record(ar, courses)
 
 
-def create_student(student):
+def create_student_in_db(student):
     create_table_if_not_exists()
     conn_string = "host='localhost' dbname='CourseEmbeddings' user='newuser' password='password'"
     conn = psycopg2.connect(conn_string)
@@ -110,10 +124,12 @@ def create_student(student):
 
     cur.execute("SELECT * FROM users WHERE eid = %s", student.eid)
     existing_student = cur.fetchone()
-    
 
+    student_id = 0
+    
     if existing_student is None:
         # Insert new student
+
         cur.execute("""
             INSERT INTO users (eid, firstname, lastname, school, major, minor, 
                     academic_record, grad_year, add_credit, qual_data)
@@ -128,12 +144,61 @@ def create_student(student):
     else:
         print("Student already exists.")
 
-    return
+    return student_id
 
 
 def udpate(student):
-    return
+    conn_string = "host='localhost' dbname='CourseEmbeddings' user='newuser' password='password'"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+
+    # Check if student exists
+    cur.execute("SELECT * FROM users WHERE eid = %s", (student.eid,))
+    existing_student = cur.fetchone()
+
+    if existing_student is not None:
+        # Update the student's information
+        cur.execute("""
+            UPDATE users
+            SET firstname = %s,
+                lastname = %s,
+                school = %s,
+                major = %s,
+                minor = %s,
+                academic_record = %s,
+                grad_year = %s,
+                add_credit = %s,
+                qual_data = %s
+            WHERE eid = %s
+        """, (student.firstname, student.lastname, student.school, 
+              majors_to_string(student.major), minors_to_string(student.minor), 
+              academic_record_to_string(student.academic_record), student.grad_year, 
+              student.add_credit, student.qual_data, student.eid))
+        conn.commit()
+        print(f"Student with EID {student.eid} updated.")
+    else:
+        print("Student not found.")
+
+    cur.close()
+    conn.close()
 
 def get_student(eagleid):
-    student = "object"
-    return student
+    conn_string = "host='localhost' dbname='CourseEmbeddings' user='newuser' password='password'"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+
+    # Fetch the student data
+    cur.execute("SELECT eid, firstname, lastname, school, major, minor, academic_record, grad_year, add_credit, qual_data FROM users WHERE eid = %s", (eid,))
+    student_data = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    if student_data is not None:
+        # Unpack the data and create a Student object
+        eid, firstname, lastname, school, major, minor, academic_record, grad_year, add_credit, qual_data = student_data
+        student = createStudent(eid, firstname, lastname, school, string_to_majors(major), string_to_minors(minor), string_to_academic_record(academic_record), grad_year, add_credit, qual_data)
+        return student
+    else:
+        print("Student not found.")
+        return None
